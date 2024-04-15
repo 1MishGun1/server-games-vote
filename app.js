837b917332bfcf7763774c88876a822b5af6e5a0
1MishGun1 <mikhail.kudrov@gmail.com>
2024-04-15 23:01:07 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const http = require("http");
+const path = require("path");
+const mimeTypes = require("./appModules/http-utils/mine-types");
+
 const staticFile = require("./appModules/http-utils/static-file");
 const server = http.createServer((req, res) => {
   const url = req.url;
@@ -7,5 +10,16 @@ const server = http.createServer((req, res) => {
       res.statusCode = 200;
       staticFile(res, "/index.html", ".html");
       break;
+
+    default:
+      const extname = String(path.extname(url)).toLowerCase();
+      if (extname in mimeTypes) {
+        staticFile(res, url, extname);
+      } else {
+        res.statusCode = 404;
+        res.end("Not Found");
+      }
   }
 });
+
+server.listen(3005);
