9f6ed71790034e7551a557cdcd5b41459e732405
1MishGun1 <mikhail.kudrov@gmail.com>
2024-04-17 22:00:48 +0300

    update vote.js

--- controllers/vote.js
+++ controllers/vote.js
@@ -1,18 +1,20 @@
+const fs = require('fs').promises;
 const parseBody = require('../appModules/http-utils/parse-body');
 
 const voteRouteController = async (req, res) => {
   if (res.method !== 200) {
     res.statusCode = 404;
     res.end('Not Found');
-    parseBody(req, (err, body) => {
-      if (err) {
-        res.statusCode = 500;
-        res.end('Internal Server Error');
-      }
+  } else {
+    try {
+      const body = await parseBody(req);
       console.log(body);
       res.statusCode = 200;
       res.end('Success!');
-    });
+    } catch (error) {
+      res.statusCode = 500;
+      res.end('Internal Server Error');
+    }
   }
 };
 
