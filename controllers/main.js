738b08ddd9ee0daa781901f7e0c35941255d2e3f
1MishGun1 <mikhail.kudrov@gmail.com>
2024-04-17 22:00:37 +0300

    update main.js

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,10 @@
 const staticFile = require('../appModules/http-utils/static-file');
+const { getData, endpoints } = require('../appModules/api');
+const { config, makeRatingFile } = require('../appModules/rating');
 
-const mainRouterController = (res, publicUrl, extname) => {
+const mainRouterController = async (res, publicUrl, extname) => {
+  const data = await getData(endpoints.games);
+  await makeRatingFile(config.PATH_TO_RATING_FILE, data);
   res.statusCode = 200;
   staticFile(res, '/index.html', '.html');
 };
