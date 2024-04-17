92975259cac465c0c287063d57dcac0d41df8f0b
1MishGun1 <mikhail.kudrov@gmail.com>
2024-04-17 11:11:45 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-const http = require("http");
-const path = require("path");
-const mimeTypes = require("./appModules/http-utils/mine-types");
+const http = require('http');
+const path = require('path');
+const mimeTypes = require('./appModules/http-utils/mine-types');
 
-const staticFile = require("./appModules/http-utils/static-file");
+const staticFile = require('./appModules/http-utils/static-file');
+const mainRouterController = require('./controllers/main');
+const defaultRouteController = require('./controllers/default');
+const gameRouteController = require('./controllers/game');
+const voteRouteController = require('./controllers/vote');
+
 const server = http.createServer((req, res) => {
   const url = req.url;
   switch (url) {
-    case "/":
-      res.statusCode = 200;
-      staticFile(res, "/index.html", ".html");
+    case '/':
+      mainRouterController(res, './index.html', '.html');
+      break;
+
+    case '/game':
+      gameRouteController(res);
+      break;
+
+    case '/vote':
+      voteRouteController(req, res);
       break;
 
     default:
-      const extname = String(path.extname(url)).toLowerCase();
-      if (extname in mimeTypes) {
-        staticFile(res, url, extname);
-      } else {
-        res.statusCode = 404;
-        res.end("Not Found");
-      }
+      defaultRouteController(res, url);
   }
 });
 
