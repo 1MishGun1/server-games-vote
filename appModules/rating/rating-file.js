fbeaa5ba52e8a62babc2cfa8f4c9aa044b350bed
1MishGun1 <mikhail.kudrov@gmail.com>
2024-04-18 12:49:11 +0300

    update rating-file.js

--- appModules/rating/rating-file.js
+++ appModules/rating/rating-file.js
@@ -4,15 +4,11 @@ const fs = require('fs').promises;
  * path — путь к файлу
  * array — список игр
  */
-async function makeRatingFile(path, array) {
-  // Читаем содержимое файла по пути path. ratingFile — строка со всем содержимым файла
+const makeRatingFile = async (path, array) => {
   const ratingFile = await fs.readFile(path, 'utf8');
-  // Так как ratingFile — строка, то преобразуем её в json
   const ratingArray = JSON.parse(ratingFile);
 
-  // Код далее повторяем для каждой игры из списка игр
   array.forEach((item) => {
-    // Если в ratingArray ещё нет игры с таким id,
     if (!ratingArray.find((el) => el.id === item.id)) {
       let obj = {
         id: item.id,
@@ -22,13 +18,11 @@ async function makeRatingFile(path, array) {
         description: item.description,
         rating: 0,
       };
-      // добавляем её к уже существующему списку игр
       ratingArray.push(obj);
     }
   });
 
-  // Обновим список игр в файле по пути path, записав туда обновлённый массив ratingArray
   await fs.writeFile(path, JSON.stringify(ratingArray));
-}
+};
 
 module.exports = makeRatingFile;
